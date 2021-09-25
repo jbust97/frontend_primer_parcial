22c54d4c97261c648b19a8eb1dce40d166518d21
jbus97 <hugofleitasbus@gmail.com>
2021-09-25 17:24:23 -0400

    Muestra los servicios de una ficha

--- src/app/service/servicio.service.ts
+++ src/app/service/servicio.service.ts
@@ -3,17 +3,22 @@ import { listadatos } from '../models/datos';
 import { Servicio } from '../models/servicio';
 import { base_url } from '../base_url';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioService {
-  private api = base_url;
+  private api = base_url + 'stock-pwfe/servicio';
   constructor(private http: HttpClient) { 
 
   }
   getServicio(itemsPerPage:number,inicio:number):Observable<listadatos<Servicio>>{
-    return this.http.get<listadatos<Servicio>>(this.api + `stock-pwfe/servicio?cantidad=${itemsPerPage}&inicio=${inicio}`);
-  } 
+    return this.http.get<listadatos<Servicio>>(this.api + `?cantidad=${itemsPerPage}&inicio=${inicio}`);
+  }
+  getServicioFicha(idFichaClinica: number):Observable<listadatos<Servicio>>{
+    let params = new HttpParams()
+    .set('ejemplo',`{"idFichaClinica":{"idFichaClinica": ${idFichaClinica}}}`);
+    return this.http.get<listadatos<Servicio>>(this.api, {params:params}); 
+  }
 }
