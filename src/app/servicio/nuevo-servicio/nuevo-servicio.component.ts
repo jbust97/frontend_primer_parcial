9ad55a45a57e5485dc8abb3e4879a3a76bf0bf65
RaulGalvan <ramako72@gmail.com>
2021-09-26 10:58:30 -0400

    post

--- src/app/servicio/nuevo-servicio/nuevo-servicio.component.ts
+++ src/app/servicio/nuevo-servicio/nuevo-servicio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Ficha } from 'src/app/models/fichas';
+import { Servicio } from 'src/app/models/servicio';
+import { ServicefichaService } from 'src/app/service/serviceficha.service';
+import { ServicioService } from 'src/app/service/servicio.service';
 
 @Component({
   selector: 'app-nuevo-servicio',
@@ -8,48 +12,27 @@ import { Component, OnInit } from '@angular/core';
 export class NuevoServicioComponent implements OnInit {
 
   ficha: Ficha = new Ficha();
-  empleado: Persona = new Persona();
-  cliente: Persona = new Persona();
-  categorias: Categoria[] = []
-  tipoProductos: Subcategoria[]= [];
-  tipoProducto: Subcategoria = new Subcategoria();
-  categoria: Categoria = new Categoria()
-  constructor(private serviceCategoria: ServicecategoriaService, private serviceTipoProducto: ServicetipoproductoService, private serviceFicha: ServicefichaService) { }
+  fichas: Ficha[] = []
+  constructor(private servicioService: ServicioService, private serviceFicha: ServicefichaService) { }
 
   ngOnInit(): void {
-    this.getCategorias()
   }
 
-  seleccionarEmpleado(empleado: Persona){
-    this.empleado = empleado
-    this.empleado.fullName = empleado.nombre + " " + empleado.apellido;
-  }
-
-  seleccionarCliente(cliente: Persona){
-    this.cliente = cliente
-    this.cliente.fullName = cliente.nombre + " " + cliente.apellido;
-  }
-  getCategorias(){
-    this.serviceCategoria.getCategorias().subscribe((data:any)=>{
-      this.categorias = data.lista;
-    })
-  }
-
-  getTipoProductos(){
-    this.serviceTipoProducto.getTipoProductos(this.categoria.idCategoria).subscribe((data:any)=>{
-      this.tipoProductos = data.lista
+  getFichas(){
+    this.serviceFicha.getfichas(5,0).subscribe((data:any)=>{
+      this.fichas = data.lista;
     })
   }
 
   guardarServicio(){
-    this.ficha.idCliente = new Persona;
+    /*this.ficha.idCliente = new Persona;
     this.ficha.idEmpleado = new Persona;
     this.ficha.idTipoProducto = new Subcategoria;
     this.ficha.idTipoProducto.idTipoProducto = this.tipoProducto.idTipoProducto
     this.ficha.idCliente.idPersona = this.cliente.idPersona
     this.ficha.idEmpleado.idPersona = this.empleado.idPersona
     this.serviceFicha.postficha(this.ficha).subscribe()
-    console.log(this.ficha)
+    console.log(this.ficha)*/
   }
 
 }
