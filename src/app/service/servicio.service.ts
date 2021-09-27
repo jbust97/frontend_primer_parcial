73cd8830e3f29998d443a407254002069bc55d73
jbus97 <hugofleitasbus@gmail.com>
2021-09-26 22:08:09 -0400

    quick fix de main

--- src/app/service/servicio.service.ts
+++ src/app/service/servicio.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { listadatos } from '../models/datos';
-import { Servicio, ServicioPostBody, DetallePostBody, Detalle } from '../models/servicio';
+import { Servicio, ServicioPostBody, DetallePostBody} from '../models/servicio';
 import { base_url } from '../base_url';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
